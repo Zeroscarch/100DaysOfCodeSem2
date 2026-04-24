f98a15c3faec5b3b79bc8ba55d40234967ddd425
Zeroscarch <anikdino7373@gmail.com>
2026-04-25 03:12:39 +0530

    day 39

---
+++ Day39.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100
+
+int heap[MAX];
+int size = 0;
+
+void insert(int x) {
+    int i = size++;
+    while (i > 0 && heap[(i - 1) / 2] > x) {
+        heap[i] = heap[(i - 1) / 2];
+        i = (i - 1) / 2;
+    }
+    heap[i] = x;
+}
+
+int extractMin() {
+    if (size == 0) return -1;
+
+    int min = heap[0];
+    int last = heap[--size];
+
+    int i = 0;
+    while (2 * i + 1 < size) {
+        int child = 2 * i + 1;
+
+        if (child + 1 < size && heap[child + 1] < heap[child])
+            child++;
+
+        if (last <= heap[child]) break;
+
+        heap[i] = heap[child];
+        i = child;
+    }
+
+    heap[i] = last;
+    return min;
+}
+
+int peek() {
+    if (size == 0) return -1;
+    return heap[0];
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    while (n--) {
+        char op[20];
+        scanf("%s", op);
+
+        if (strcmp(op, "insert") == 0) {
+            int x;
+            scanf("%d", &x);
+            insert(x);
+        }
+        else if (strcmp(op, "extractMin") == 0) {
+            printf("%d\n", extractMin());
+        }
+        else if (strcmp(op, "peek") == 0) {
+            printf("%d\n", peek());
+        }
+    }
+
+    return 0;
+}
